e314c9199c20f540165b01c70b1884d50f7c9dfb
Amari Ellington <amari.l.ellington@gmail.com>
2019-04-13 15:11:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,3 +7,13 @@ array.each do |string|
 end
   narray.join(" ")
 end
+
+def reverse_each_word
+  reverse_each_word(string)
+  array = string.split(" ") #tunr string into an array
+  test_array = []
+  array.collect do|string|
+    test_array << string.reverse #reverse each word in the array
+  end
+  test_array.join(" ")
+end
